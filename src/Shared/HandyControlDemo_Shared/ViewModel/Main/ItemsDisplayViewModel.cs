857d723a4920bdc7f73f323bf0cde7c86a97a667
NaBian <836904362@qq.com>
2025-09-14 11:50:55 +0800

    chore: misc modify.

--- src/Shared/HandyControlDemo_Shared/ViewModel/Main/ItemsDisplayViewModel.cs
+++ src/Shared/HandyControlDemo_Shared/ViewModel/Main/ItemsDisplayViewModel.cs
@@ -10,7 +10,7 @@ public class ItemsDisplayViewModel : DemoViewModelBase<AvatarModel>
     public ItemsDisplayViewModel(Func<List<AvatarModel>> getDataAction)
     {
 #if NET40
-            Task.Factory.StartNew(() => DataList = getDataAction?.Invoke()).ContinueWith(obj => DataGot = true);
+        Task.Factory.StartNew(() => DataList = getDataAction?.Invoke()).ContinueWith(obj => DataGot = true);
 #else
         Task.Run(() => DataList = getDataAction?.Invoke()).ContinueWith(obj => DataGot = true);
 #endif
@@ -22,7 +22,7 @@ public class ItemsDisplayViewModel : DemoViewModelBase<AvatarModel>
     {
         get => _dataGot;
 #if NET40
-            set => Set(nameof(DataGot), ref _dataGot, value);
+        set => Set(nameof(DataGot), ref _dataGot, value);
 #else
         set => Set(ref _dataGot, value);
 #endif
